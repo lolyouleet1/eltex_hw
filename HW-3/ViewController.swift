c19f90f4e5501fd2ad134102f51dd41c17dedb1f
Roman Prokhorov <roman@Romans-Mac-mini.local>
2026-03-17 16:06:15 +0700

    HW-3 ready

---
+++ HW-3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW-3
+//
+//  Created by Roman Prokhorov on 17.03.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
